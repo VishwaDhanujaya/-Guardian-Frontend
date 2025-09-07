7b119d8b743d9e000f3c1c07fae56f0ea89d4622
Vishwa Dhanujaya <58469705+VishwaDhanujaya@users.noreply.github.com>
2025-09-07 20:29:05 +0530

    refine colors and simplify safety alerts

--- global.d.ts
+++ global.d.ts
@@ -1,27 +1,31 @@
 // global.d.ts
-declare module "*.png" {
-  const src: import("react-native").ImageSourcePropType;
+declare module '*.png' {
+  const src: import('react-native').ImageSourcePropType;
   export default src;
 }
-declare module "*.jpg" {
-  const src: import("react-native").ImageSourcePropType;
+declare module '*.jpg' {
+  const src: import('react-native').ImageSourcePropType;
   export default src;
 }
-declare module "*.jpeg" {
-  const src: import("react-native").ImageSourcePropType;
+declare module '*.jpeg' {
+  const src: import('react-native').ImageSourcePropType;
   export default src;
 }
-declare module "*.gif" {
-  const src: import("react-native").ImageSourcePropType;
+declare module '*.gif' {
+  const src: import('react-native').ImageSourcePropType;
   export default src;
 }
-declare module "*.webp" {
-  const src: import("react-native").ImageSourcePropType;
+declare module '*.webp' {
+  const src: import('react-native').ImageSourcePropType;
   export default src;
 }
-declare module "*.svg" {
-  import * as React from "react";
-    import { SvgProps } from "react-native-svg";
+declare module '*.svg' {
+  import * as React from 'react';
+  import { SvgProps } from 'react-native-svg';
   const content: React.FC<SvgProps>;
   export default content;
 }
+
+declare module '@react-native-async-storage/async-storage';
+declare module 'expo-symbols';
+declare module 'expo-blur';
